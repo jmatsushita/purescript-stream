d8d1f1f4ea81e95f8eda9e80d7895d9dd0abaa23
Jun Matsushita <jun@iilab.org>
2021-07-20 22:58:18 +0200

    Cleanup tests with failing lazy functor instance

--- test/Main.purs
+++ test/Main.purs
@@ -28,29 +28,12 @@ functorLazy = map f $ streamBoom
 functor :: Stream Int
 functor = map (add 1) stream0N
 
--- data Store s a = Store a (a -> s)
-
-type Index = Int -- Naturals
-
-myStore :: Store Index Char
-myStore = store (\i -> unsafePartial $ unsafeIndex ['a','b','c'] i) 0
-
--- instance extendStore :: Extend (Store s a) where
---   extend f = map f <<< duplicate
---     where
---       duplicate (Store i f) = Store i ()
 
 main :: Effect Unit
 main = do
   log "🍝"
-  -- logShow $ pos myStore
-  -- logShow $ peek (pos myStore) myStore
-  logShow $ extract myStore
-  logShow $ extract $ seek 2 myStore
-  logShow $ extract $ extract $ (duplicate myStore :: Store Index (Store Index Char))
-  logShow $ extract $ extend (\w -> [extract w, extract $ seek (add 1 $ pos w) w] <> ['!']) myStore 
-  -- logShow $ take 3 functor == [1,2,3]
-  -- logShow $ extract $ functor
-  -- logShow $ extract $ extract $ duplicate $ functor
-  -- logShow $ take 3 functorLazy == [0,0,0]
+  logShow $ take 3 functor == [1,2,3]
+  logShow $ extract $ functor
+  logShow $ extract $ extract $ duplicate $ functor
+  logShow $ take 3 functorLazy == [0,0,0]
   log "You should add some tests."
